badb0a065463ad134fa5ae49dd72f81df1c6bd56
Ankush Singh <98583838+ankush152002@users.noreply.github.com>
2024-10-17 11:08:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+// app.js
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.get('/', (req, res) => {
+    res.send('Hello World from App 1!!');
+});
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+});
